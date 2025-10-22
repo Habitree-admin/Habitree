1a7425c43541d8b7a98adcaf32df0b3cdc7beaed
A01712317 <A01712317@tec.mx>
2025-10-21 20:22:33 -0600

    feat comentarios para editar y desactivar una notificación

--- controllers/notifications/notification.controller.js
+++ controllers/notifications/notification.controller.js
@@ -56,11 +56,17 @@ exports.createNotification = async (req, res) => {
     }
 };
 
-
+/**
+ * 
+ * This function bring and show the information of a notification
+ * 
+ * getNotificationEditor handles GET requests to show de pop-up
+ */
 exports.getNotificationEditor = async (req, res) => {
     const { id } = req.params;
 
     try {
+        // Consult in the DB the selected notification
         const [rows] = await Notification.fetchById(id);
 
         if (!rows || rows.length === 0) {
@@ -69,7 +75,7 @@ exports.getNotificationEditor = async (req, res) => {
 
         const notification = rows[0];
 
-        // Pass all fields including title and channel
+        // Pass all fields from de DB to the view
         res.render('notifications/editNotifications', {
             id: notification.IDNotification,
             titulo: notification.titulo,
@@ -99,15 +105,22 @@ exports.postAddNotification = async (req, res) => {
     }
 };
 
+
+/**
+ * 
+ * This function deactivate an active notification
+ * 
+ * postDelete handles POST requests to update de DB
+ */
 exports.postDelete = (req, res) => {
     const { id, currentState } = req.body;
 
-    // If the current state is 'deactivate' (i.e., it is active), change it to 0.
-    // If the current state is 'activate' (i.e., it is inactive), change it to 1.
+    // If the current state is 'deactivate', change it to 0.
+    // If the current state is 'activate', change it to 1.
     const newIsActive = currentState === 'deactivate' ? 0 : 1;
 
     try {
-        // Call the model function with the new numeric value
+        // Call the model function with the new state
         Notification.updateIsActive(id, newIsActive);
         
         res.redirect('/notifications');
@@ -120,15 +133,26 @@ exports.postDelete = (req, res) => {
     }
 };
 
+
+/**
+ * 
+ * This function update an notification
+ * 
+ * postUpdate handles POST requests to update the notification content
+ */
 exports.postUpdate = async (req, res) => {
+    // Extract the content  sent by the view
   const { id, canal, titulo, mensaje, category } = req.body;
   try {
+    // Update the notificication content with the new one
     await Notification.update(titulo, mensaje, canal, category, id);
     console.log("Success update");
 
+    // Send the notification with the changes 
     await sendNotificationToTopic(canal, titulo, mensaje);
     console.log("Success: Notificación enviada por FCM");
 
+    // Redirect the user after processing de changes
     res.redirect('/notifications');
   } catch (error) {
     console.error("Error al actualizar la notificación:", error);
